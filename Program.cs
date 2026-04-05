274f5c5e1e13d243b0d827595a7e62eebaca3ded
priyanka5123 <priyanka.k8100@gmail.com>
2026-04-05 09:50:09 -0700

    added Divide Numbers

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using System.Drawing;
 using Pastel;
-Console.WriteLine("Hello, World!".Pastel(ConsoleColor.Green));
+
+public class Program
+{
+    public static int DivideNumbers(int numerator, int denominator)
+    {
+        if (denominator == 0)
+        {
+            Console.WriteLine("Error: Division by zero is not allowed.");
+            return 0;
+        }
+        int result = (numerator/denominator);
+        return result;
+    }
+    public static void Main()
+    {
+        int result = DivideNumbers(10, 0);
+        Console.WriteLine("The result is: " + result);
+        Console.WriteLine("Hello, World!".Pastel(ConsoleColor.Green));
+
+    }
+}
