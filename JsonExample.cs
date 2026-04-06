b52981fc06bf87aaa6af34d332fb6c9763c96cad
priyanka5123 <priyanka.k8100@gmail.com>
2026-04-06 15:14:52 -0700

    added JsonExample

---
+++ JsonExample.cs
@@ -0,0 +1,6 @@
+using System;
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Drawing;
 using System.Xml.Schema;
 using Pastel;
+using Newtonsoft.Json;
 
 public class Program
 {
@@ -61,5 +62,15 @@ public class Program
                     break;
             }
         }
+
+        // Deserialize JSON to Person object
+        string json = "{\"Name\": \"John Doe\", \"Age\": 30}";
+        Person person = JsonConvert.DeserializeObject<Person>(json);
+        Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+        // Serialize Person object to JSON
+        Person newPerson = new Person { Name = "Ping Jeong", Age = 25 };
+        string newJson = JsonConvert.SerializeObject(newPerson);
+        Console.WriteLine($"Serialized JSON: {newJson}");
     }
+
 }
